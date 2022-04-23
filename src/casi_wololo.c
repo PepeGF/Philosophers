f3b883a53bf1579a19c773d8d09ab15aa6c18b53
Pepe <pepe@Pepes-Mac.local>
2022-04-23 23:54:22 +0200

    Philo double linked list created

---
+++ src/casi_wololo.c
@@ -0,0 +1,21 @@
+#include "../inc/philo.h"
+
+/*
+int	ft_lst_len(t_philo *lst_philo)
+{
+	t_philo	*aux;
+	int		i;
+
+	if (!lst_philo)
+		return (0);
+	i = 0;
+	aux = lst_philo;
+	while (aux)
+	{
+		aux = aux->right;
+		i++;
+	}
+	printf("Longitud lista: %d\n", i);
+	return (i);
+}
+*/
--- src/main.c
+++ src/main.c
@@ -24,23 +24,53 @@ void	ft_create_philos(t_philo **lst_philo, t_args args)
 	i = 0;
 	while (i < args.n_philo)
 	{
-		ft_lstadd_back(lst_philo, ft_lstnew(i));
+		ft_lstadd_back(lst_philo, ft_lstnew(i + 1));
 		i++;
 	}
 	return ;
 }
 
-void	ft_free_philos(t_philo *lst_philo)
+void	ft_link_list(t_philo *lst_philo)
 {
-	t_philo	*aux;
-	t_philo	*aux2;
+	t_philo *aux;
 
 	aux = lst_philo;
 	while (aux)
 	{
+		if (aux->right)
+			aux->right->left = aux;
+		else
+		{
+			lst_philo->left = aux;
+			aux->right = lst_philo;
+			break;
+		}
+		aux = aux->right;
+	}
+	aux = lst_philo;
+/*	int i = 1;
+	while (i <= 20)
+	{
+		printf("Filo nº: %d, filo de la dcha: %d\n", aux->id, aux->right->id);
+		i++;
+		aux = aux->right;
+	}*/
+}
+
+void	ft_free_philos(t_philo *lst_philo, t_args args)
+{
+	t_philo	*aux;
+	t_philo	*aux2;
+	int		i;
+
+	i = 1;
+	aux = lst_philo;
+	while (i < args.n_philo)
+	{
 		aux2 = aux;
 		aux = aux->right;
 		free(aux2);
+		i++;
 	}
 	free(aux);
 	return ;
@@ -50,11 +80,13 @@ int	main(int argc, char *argv[])
 {
 	t_args	args;
 	t_philo	*lst_philo;
-atexit(leaks);
+ atexit(leaks);
 	if (!arg_number_manage(argc) || !atoi_args(argv, &args))
 		return (1);
 	lst_philo = 0;
 	ft_create_philos(&lst_philo, args);
-	ft_free_philos(lst_philo);
+	// ft_lst_len(lst_philo);
+	ft_link_list(lst_philo);
+	ft_free_philos(lst_philo, args);
 	return (0);
 }
